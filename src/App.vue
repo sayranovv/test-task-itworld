181716cad6050c43551bac798f97637152c13afb
sayranovv <sayranoveldar@gmail.com>
2025-07-26 23:09:14 +0500

    Create app-sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+</script>
 
-<template></template>
-
-<style scoped></style>
+<template>
+  <div>
+    <SidebarProvider>
+      <AppSidebar />
+      <div class="flex-1 overflow-hidden pr-2">
+        <SidebarTrigger class="ml-4 mt-4" />
+        <RouterView />
+      </div>
+    </SidebarProvider>
+  </div>
+</template>
