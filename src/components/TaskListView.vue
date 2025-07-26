da30c647542f308652bc4077c9a47af8db0e6556
sayranovv <sayranoveldar@gmail.com>
2025-07-26 23:12:08 +0500

    Create tasks list view component

---
+++ src/components/TaskListView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TaskFilters from './TaskFilters.vue'
+import TasksList from './TasksList.vue'
+</script>
+
+<template>
+  <div>
+    <TaskFilters />
+    <TasksList />
+  </div>
+</template>
