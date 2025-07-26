0679e9d651efaf0390ae103a95340e2967a29c3a
sayranovv <sayranoveldar@gmail.com>
2025-07-26 23:11:44 +0500

    Create tasks list component

---
+++ src/components/TasksList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TaskItem from '@/components/TaskItem.vue'
+</script>
+
+<template>
+  <h2 class="pt-5 pb-3 text-2xl font-bold">Tasks</h2>
+  <TaskItem
+    v-for="_ in 10"
+    :key="_"
+    title="Task example"
+    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, perspiciatis."
+  />
+</template>
