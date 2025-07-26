d089cc52b8fb6a62507bd584f4e547596c42036c
sayranovv <sayranoveldar@gmail.com>
2025-07-26 23:11:26 +0500

    Create multiple select component

---
+++ src/components/TaskFilterMultipleSelect.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+defineProps<{
+  items: string[]
+  placeholder?: string
+  label?: string
+}>()
+</script>
+
+<template>
+  <Select multiple>
+    <SelectTrigger>
+      <SelectValue v-if="placeholder" :placeholder="placeholder" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup>
+        <SelectLabel v-if="label">{{ label }}</SelectLabel>
+        <SelectItem v-for="item in items" :value="item.toLowerCase()"> {{ item }} </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+</template>
