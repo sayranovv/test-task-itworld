cfbd4688038b9606ae892f778d24ed36a3640e12
sayranovv <sayranoveldar@gmail.com>
2025-07-27 00:35:20 +0500

    Remove description from task item

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -2,19 +2,19 @@
 import { Circle, ChevronRight } from 'lucide-vue-next'
 defineProps<{
   title: string
-  description: string
+  // description: string
 }>()
 </script>
 
 <template>
   <div class="py-3 flex justify-between items-start gap-4">
     <div class="flex gap-4 flex-1 overflow-hidden">
-      <div class="size-12 shrink-0 bg-primary/10 rounded-lg flex justify-center items-center">
+      <div class="size-12 shrink-0 bg-primary/5 rounded-lg flex justify-center items-center">
         <Circle />
       </div>
       <div class="flex flex-col justify-center overflow-hidden">
         <p class="font-medium">{{ title }}</p>
-        <p class="text-sm opacity-50 truncate">{{ description }}</p>
+        <!--        <p v-if="description" class="text-sm opacity-50 truncate">{{ description }}</p>-->
       </div>
     </div>
     <ChevronRight class="size-7 shrink-0" />
