34d79d44e71f67abe7c02e08c5a09b27c1519f97
sayranovv <sayranoveldar@gmail.com>
2025-07-26 23:11:35 +0500

    Create task item component

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Circle, ChevronRight } from 'lucide-vue-next'
+defineProps<{
+  title: string
+  description: string
+}>()
+</script>
+
+<template>
+  <div class="py-3 flex justify-between items-start gap-4">
+    <div class="flex gap-4 flex-1 overflow-hidden">
+      <div class="size-12 shrink-0 bg-primary/10 rounded-lg flex justify-center items-center">
+        <Circle />
+      </div>
+      <div class="flex flex-col justify-center overflow-hidden">
+        <p class="font-medium">{{ title }}</p>
+        <p class="text-sm opacity-50 truncate">{{ description }}</p>
+      </div>
+    </div>
+    <ChevronRight class="size-7 shrink-0" />
+  </div>
+</template>
