1014da858bc8ce488c01ff363dc3f6a6174cc1c2
sayranovv <sayranoveldar@gmail.com>
2025-07-28 17:06:55 +0500

    Add inline info for task

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,23 +1,36 @@
 <script setup lang="ts">
-import { Circle, ChevronRight, CircleCheck, CircleDotDashed } from 'lucide-vue-next'
+import { Circle, CircleCheck, Clock, X } from 'lucide-vue-next'
+import { Badge } from '@/components/ui/badge'
 import type { Task } from '@/types/models.ts'
+import CreateTaskDialog from '@/components/CreateTaskDialog.vue'
+
 defineProps<{ task: Task }>()
 </script>
 
 <template>
   <div class="py-3">
     <div class="flex justify-between items-start gap-4">
-      <div class="flex gap-4 flex-1 overflow-hidden">
+      <div class="flex gap-4 flex-1 overflow-hidden items-center">
         <div class="size-12 shrink-0 bg-primary/5 rounded-lg flex justify-center items-center">
           <CircleCheck v-if="task.status === 'done'" />
-          <CircleDotDashed v-else-if="task.status === 'todo'" />
-          <Circle v-else />
+          <Circle v-else-if="task.status === 'todo'" />
+          <Clock v-else />
         </div>
         <div class="flex flex-col justify-center overflow-hidden">
           <p class="font-medium">{{ task.title }}</p>
         </div>
+
+        <Badge>{{ task.status }}</Badge>
+
+        <div class="flex gap-1">
+          <Badge v-for="tag in task.tags" variant="outline">{{ tag }}</Badge>
+        </div>
       </div>
-      <ChevronRight class="size-7 shrink-0" />
+      <div class="flex gap-4">
+        <CreateTaskDialog button-title="Добавить подзадачу" />
+
+        <X class="size-7 shrink-0" />
+      </div>
     </div>
     <div v-if="task.subtasks.length" class="ml-5 space-y-2">
       <TaskItem v-for="subtask in task.subtasks" :key="subtask.id" :task="subtask" />
