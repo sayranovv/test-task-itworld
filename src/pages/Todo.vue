b52356abdfada90a37344f6ad5e173bbc7eb1158
sayranovv <sayranoveldar@gmail.com>
2025-07-28 17:02:26 +0500

    Create pages

---
+++ src/pages/Todo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TasksList from '@/components/TasksList.vue'
+import { useTasksStore } from '@/stores/tasksStore.ts'
+import { computed } from 'vue'
+
+const tasksStore = useTasksStore()
+
+const filtered = computed(() => tasksStore.filterTasksByStatus('todo', true))
+</script>
+
+<template>
+  <div class="p-4">
+    <header class="space-y-3 mb-4">
+      <h1 class="text-3xl font-bold">К выполнению</h1>
+      <p class="text-sm opacity-50">Задачи, ожидающие выполнения</p>
+    </header>
+    <main>
+      <TasksList :tasks="filtered" />
+    </main>
+  </div>
+</template>
