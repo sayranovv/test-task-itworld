b124f20fb62d13b482e1b42074ed40d9577962cc
sayranovv <sayranoveldar@gmail.com>
2025-07-28 17:03:26 +0500

    Add toaster component to app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import AppSidebar from '@/components/AppSidebar.vue'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { Toaster } from '@/components/ui/sonner'
+import 'vue-sonner/style.css'
 </script>
 
 <template>
@@ -10,6 +12,7 @@ import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
       <div class="flex-1 overflow-hidden pr-2">
         <SidebarTrigger class="ml-4 mt-4" />
         <RouterView />
+        <Toaster />
       </div>
     </SidebarProvider>
   </div>
