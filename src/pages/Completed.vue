928cff1cecdfbb891612a1b64727bd6f7755e470
sayranovv <sayranoveldar@gmail.com>
2025-07-28 17:05:32 +0500

    Add UI for page

--- src/pages/Completed.vue
+++ src/pages/Completed.vue
@@ -1,3 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TasksList from '@/components/TasksList.vue'
+import { useTasksStore } from '@/stores/tasksStore.ts'
+import { computed } from 'vue'
 
-<template>Completed page</template>
+const tasksStore = useTasksStore()
+
+const filtered = computed(() => tasksStore.filterTasksByStatus('done', true))
+</script>
+
+<template>
+  <div class="p-4">
+    <header class="space-y-3 mb-4">
+      <h1 class="text-3xl font-bold">Завершенные</h1>
+      <p class="text-sm opacity-50">Выполненные задачи</p>
+    </header>
+    <main>
+      <TasksList :tasks="filtered" />
+    </main>
+  </div>
+</template>
